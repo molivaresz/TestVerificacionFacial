422ba9fb157bff4a185d96e138c7f26d9a94e9c8
molivaresz <molivaresz1@gmail.com>
2025-01-31 16:29:20 -0300

    Add: para tomar el Liveness

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -20,6 +20,20 @@ const Home = () => {
         }
     };
 
+    const Selfie = async (IdSesion) => {
+        try {
+            TOCautocapture('containerautocaptura', {
+            locale: "es",
+            session_id: IdSesion,
+            callback: function(liveness_token, image){ document.getElementById("Tliveness").value = liveness_token },
+            failure: function(error){ alert(error); }
+        });
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+
     return (
     <>
     <div className='col container-fluid d-flex pt-5 ps-5'>
@@ -33,9 +47,19 @@ const Home = () => {
                 <label className="form-label">Token Back</label>
                 <input type="text" className="form-control" id="Tback" />
             </div>
-            <button type="button" onClick={() => CapturaDoc(document.getElementById("IdSesion").value,"front")} className="btn btn-outline-primary m-2">Captura Front</button>
-            <button type="button" onClick={() => CapturaDoc(document.getElementById("IdSesion").value,"back")} className="btn btn-outline-info">Captura Back</button>
+            <button type="button" onClick={() => CapturaDoc(document.getElementById("IdSesion").value,"front")} className="btn btn-outline-blue-800 m-2">Captura Front</button>
+            <button type="button" onClick={() => CapturaDoc(document.getElementById("IdSesion").value,"back")} className="btn btn-outline-cyan-800">Captura Back</button>
         </form>    
+        <form>
+            <div className="mb-3">
+                <h2>LIVENESS</h2>
+                <label className="form-label">ID Sesion</label>
+                <input type="text" className="form-control" id="IdSesion" />
+                <label className="form-label">Token Liveness</label>
+                <input type="text" className="form-control" id="Tliveness" />
+            </div>
+            <button type="button" onClick={() => Selfie(document.getElementById("IdSesion").value)} className="btn btn-outline-green-800 m-2">Liveness</button>
+        </form>
     </div>
 
     <div className="cuadrocaptura" id='containerautocaptura'></div>
